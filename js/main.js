9939dc1ba3da216f048a94680f412de737fdd3a8
Ingrid <hi@ingridcheung.com>
2012-11-10 14:34:25 -0500

    Doing stuff.

---
+++ js/main.js
@@ -0,0 +1,21 @@
+(function(){
+    var avatar_layer, avatar, background_layer, loop, stage, string;
+    stage : new Kinetic.Stage({
+        container :'screenContainer',
+        width : 1024,
+        height : 768
+    }),
+    background_layer : new Kinetic.Layer(),
+    notes_layer : new Kinetic.Layer(),
+    avatar_layer : new Kinetic.Layer(),
+
+
+    stage.add(background);
+    stage.add(notes);
+    stage.add(avatar);
+
+    loop = setTimout(function(){
+        
+    });
+
+})();
